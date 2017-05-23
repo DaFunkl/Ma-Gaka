62443c0c48078b29cb9f57e12e24b221feb25495
Alexey Gasevic <gasevic.alexey@gmail.com>
2017-05-23 13:46:57 +0200

    adding comments

--- src/wassabi/Mat.java
+++ src/wassabi/Mat.java
@@ -1,21 +1,46 @@
 package wassabi;
 
+/**
+ * 
+ * @author PakaBelly
+ *
+ * mat is represented as m x n Matrix
+ * --> int[m][n]
+ */
 public class Mat {
 	int n;
 	int m;
 	int[][] mat;
 
+	/**
+	 * constructor empty m x n matrix
+	 * @param i
+	 */
 	public Mat(int i) {
 		n = i;
+		m = i;
 		mat = new int[i][i];
 	}
 
+	/**
+	 * constructor m x n  and inits it with mat
+	 * use with care, since it's not looking for faulty input
+	 * @param n
+	 * @param m
+	 * @param mat
+	 */
 	public Mat(int n, int m, int[][] mat) {
 		this.mat = mat;
 		this.m = m;
 		this.n = n;
 	}
 
+	/**
+	 * constructor for permutation matrix
+	 * p contains the pasitions of 1 for matrix
+	 * 1 are set from top to buttom
+	 * @param p
+	 */
 	public Mat(int[] p) {
 		n = p.length;
 		mat = new int[n][n];
@@ -25,22 +50,43 @@ public class Mat {
 		}
 	}
 
+	/**
+	 * multiplies this matrix with b
+	 * @param b
+	 * @return
+	 */
 	public Mat mult(Mat b) {
 		// asdasdasdad nonsesensensnesnense
 		Mat ret = new Mat(b.mat[0]);
-		
-		
+
 		return ret;
 	}
 
-	public Mat inv() {
-		int [][] x = mat;
-		int[] px = new int[n];
-		for(int i = 0; i < n; i++){
-			px[i] = i;
+	/**
+	 * creates identity matrix with this matrix dimensions
+	 * @param n
+	 * @return
+	 */
+	public static Mat identity(int n){
+		int[] p = new int[n];
+		for(int  i = 0; i < n; i++){
+			p[i] = i;
 		}
-		// das ist die Einheitsmatrix, welche umgeformt werden soll, sodass das inverse entsteht
-		Mat p = new Mat(px);
+		return new Mat(p);
+	}
+
+	/**
+	 * return invers of this matrix
+	 * @param n
+	 * @return
+	 */
+	public Mat inv() {
+		int[][] x = mat;
+		Mat p = identity(n);
+
+		for(int i = 0; i < n; i++){
+			
+		}
 		
 		return p;
 	}
